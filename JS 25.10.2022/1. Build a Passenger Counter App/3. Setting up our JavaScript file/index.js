9949db57ab4d911331a9f72aaad6ba3fdcbb0a9d
Tomek <smuga2505tomek>
2022-10-25 11:36:10 +0200

    calculated human to dog age

---
+++ JS 25.10.2022/1. Build a Passenger Counter App/3. Setting up our JavaScript file/index.js
@@ -0,0 +1,16 @@
+//document.getElementById("count-el").innerText = 5;
+
+// let count = 5
+// let secondBatch = 7
+
+// let firstBatch = count + secondBatch
+
+// console.log(count)
+
+let myAge = 24
+let humanDogRatio = 7
+
+myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
